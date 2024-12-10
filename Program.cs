c8e4226ed17b6d6a85f09a6327a277f09284097a
Jan Marques <jan@janmarques.net>
2024-12-10 19:22:55 +0100

    d14p1

--- Program.cs
+++ Program.cs
@@ -1,123 +1,51 @@
 ﻿var fullInput =
-@"Alice would lose 57 happiness units by sitting next to Bob.
-Alice would lose 62 happiness units by sitting next to Carol.
-Alice would lose 75 happiness units by sitting next to David.
-Alice would gain 71 happiness units by sitting next to Eric.
-Alice would lose 22 happiness units by sitting next to Frank.
-Alice would lose 23 happiness units by sitting next to George.
-Alice would lose 76 happiness units by sitting next to Mallory.
-Bob would lose 14 happiness units by sitting next to Alice.
-Bob would gain 48 happiness units by sitting next to Carol.
-Bob would gain 89 happiness units by sitting next to David.
-Bob would gain 86 happiness units by sitting next to Eric.
-Bob would lose 2 happiness units by sitting next to Frank.
-Bob would gain 27 happiness units by sitting next to George.
-Bob would gain 19 happiness units by sitting next to Mallory.
-Carol would gain 37 happiness units by sitting next to Alice.
-Carol would gain 45 happiness units by sitting next to Bob.
-Carol would gain 24 happiness units by sitting next to David.
-Carol would gain 5 happiness units by sitting next to Eric.
-Carol would lose 68 happiness units by sitting next to Frank.
-Carol would lose 25 happiness units by sitting next to George.
-Carol would gain 30 happiness units by sitting next to Mallory.
-David would lose 51 happiness units by sitting next to Alice.
-David would gain 34 happiness units by sitting next to Bob.
-David would gain 99 happiness units by sitting next to Carol.
-David would gain 91 happiness units by sitting next to Eric.
-David would lose 38 happiness units by sitting next to Frank.
-David would gain 60 happiness units by sitting next to George.
-David would lose 63 happiness units by sitting next to Mallory.
-Eric would gain 23 happiness units by sitting next to Alice.
-Eric would lose 69 happiness units by sitting next to Bob.
-Eric would lose 33 happiness units by sitting next to Carol.
-Eric would lose 47 happiness units by sitting next to David.
-Eric would gain 75 happiness units by sitting next to Frank.
-Eric would gain 82 happiness units by sitting next to George.
-Eric would gain 13 happiness units by sitting next to Mallory.
-Frank would gain 77 happiness units by sitting next to Alice.
-Frank would gain 27 happiness units by sitting next to Bob.
-Frank would lose 87 happiness units by sitting next to Carol.
-Frank would gain 74 happiness units by sitting next to David.
-Frank would lose 41 happiness units by sitting next to Eric.
-Frank would lose 99 happiness units by sitting next to George.
-Frank would gain 26 happiness units by sitting next to Mallory.
-George would lose 63 happiness units by sitting next to Alice.
-George would lose 51 happiness units by sitting next to Bob.
-George would lose 60 happiness units by sitting next to Carol.
-George would gain 30 happiness units by sitting next to David.
-George would lose 100 happiness units by sitting next to Eric.
-George would lose 63 happiness units by sitting next to Frank.
-George would gain 57 happiness units by sitting next to Mallory.
-Mallory would lose 71 happiness units by sitting next to Alice.
-Mallory would lose 28 happiness units by sitting next to Bob.
-Mallory would lose 10 happiness units by sitting next to Carol.
-Mallory would gain 44 happiness units by sitting next to David.
-Mallory would gain 22 happiness units by sitting next to Eric.
-Mallory would gain 79 happiness units by sitting next to Frank.
-Mallory would lose 16 happiness units by sitting next to George.";
+@"";
 
 var smallInput =
-@"Alice would gain 54 happiness units by sitting next to Bob.
-Alice would lose 79 happiness units by sitting next to Carol.
-Alice would lose 2 happiness units by sitting next to David.
-Bob would gain 83 happiness units by sitting next to Alice.
-Bob would lose 7 happiness units by sitting next to Carol.
-Bob would lose 63 happiness units by sitting next to David.
-Carol would lose 62 happiness units by sitting next to Alice.
-Carol would gain 60 happiness units by sitting next to Bob.
-Carol would gain 55 happiness units by sitting next to David.
-David would gain 46 happiness units by sitting next to Alice.
-David would lose 7 happiness units by sitting next to Bob.
-David would gain 41 happiness units by sitting next to Carol.";
+@"Vixen can fly 8 km/s for 8 seconds, but then must rest for 53 seconds.
+Blitzen can fly 13 km/s for 4 seconds, but then must rest for 49 seconds.
+Rudolph can fly 20 km/s for 7 seconds, but then must rest for 132 seconds.
+Cupid can fly 12 km/s for 4 seconds, but then must rest for 43 seconds.
+Donner can fly 9 km/s for 5 seconds, but then must rest for 38 seconds.
+Dasher can fly 10 km/s for 4 seconds, but then must rest for 37 seconds.
+Comet can fly 3 km/s for 37 seconds, but then must rest for 76 seconds.
+Prancer can fly 9 km/s for 12 seconds, but then must rest for 97 seconds.
+Dancer can fly 37 km/s for 1 seconds, but then must rest for 36 seconds.";
 
 var smallest = "";
 
 var input = smallInput;
-input = fullInput;
+//input = fullInput;
 //input = smallest;
 var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
-var distances = new Dictionary<(string, string), int>();
+var reindeers = new Dictionary<(string name, int speed, int duration, int rest), int>();
 
-foreach (var line in input.Replace(" would lose ", " -").Replace(" would gain", "").Replace(" happiness units by sitting next to", "").Replace(".", "").Split(Environment.NewLine))
+foreach (var line in input.Replace(" can fly", "").Replace(" km/s for", "").Replace(" seconds, but then must rest for", "").Replace(" seconds.", "").Split(Environment.NewLine))
 {
     var split = line.Split(' ');
-    var key = (split[0], split[2]);
-    var value = int.Parse(split[1]);
-    distances.Add(key, value);
-}
-var cities = distances.Keys.SelectMany(key => new[] { key.Item1, key.Item2 }).Distinct().ToList();
-
-foreach (var city in cities)
-{
-    distances.Add((city, "me"), 0);
-    distances.Add(("me", city), 0);
-}
-cities.Add("me");
-
-var permutations = GetPermutations(cities, cities.Count).Select(x => x.ToArray()).ToList();
-IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-{
-    if (length == 1) return list.Select(t => new T[] { t });
-
-    return GetPermutations(list, length - 1)
-        .SelectMany(t => list.Where(e => !t.Contains(e)),
-            (t1, t2) => t1.Concat(new T[] { t2 }));
+    var name = split[0];
+    var numbers = split.Skip(1).Select(int.Parse).ToArray();
+    reindeers.Add((name, numbers[0], numbers[1], numbers[2]), 0);
 }
 
-result = permutations.Select(GetDistance).Max();
-int GetDistance(string[] cities)
+for (int i = 0; i < 2503; i++)
 {
-    var count = 0;
-    for (var i = 0; i < cities.Length; i++)
+    foreach (var reindeer in reindeers)
     {
-        count += distances[(cities[i], cities[(i + 1) % cities.Length])];
-        count += distances[(cities[(i + 1) % cities.Length], cities[i])];
+        var key = reindeer.Key;
+        var isFlying = i % (key.duration + key.rest) < key.duration;
+        if (isFlying)
+        {
+            reindeers[reindeer.Key] += key.speed;
+        }
     }
-    return count;
 }
+
+result = reindeers.Max(x => x.Value);
+
 timer.Stop();
 Console.WriteLine(result);
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
