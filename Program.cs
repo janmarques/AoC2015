87c51432f86036b77f25202da4e67ac0d63df1ed
Jan Marques <jan@janmarques.net>
2024-12-10 17:21:20 +0100

    d10p1

--- Program.cs
+++ Program.cs
@@ -1,37 +1,8 @@
 ﻿var fullInput =
-@"Tristram to AlphaCentauri = 34
-Tristram to Snowdin = 100
-Tristram to Tambi = 63
-Tristram to Faerun = 108
-Tristram to Norrath = 111
-Tristram to Straylight = 89
-Tristram to Arbre = 132
-AlphaCentauri to Snowdin = 4
-AlphaCentauri to Tambi = 79
-AlphaCentauri to Faerun = 44
-AlphaCentauri to Norrath = 147
-AlphaCentauri to Straylight = 133
-AlphaCentauri to Arbre = 74
-Snowdin to Tambi = 105
-Snowdin to Faerun = 95
-Snowdin to Norrath = 48
-Snowdin to Straylight = 88
-Snowdin to Arbre = 7
-Tambi to Faerun = 68
-Tambi to Norrath = 134
-Tambi to Straylight = 107
-Tambi to Arbre = 40
-Faerun to Norrath = 11
-Faerun to Straylight = 66
-Faerun to Arbre = 144
-Norrath to Straylight = 115
-Norrath to Arbre = 135
-Straylight to Arbre = 127";
+@"1113222113";
 
 var smallInput =
-@"London to Dublin = 464
-London to Belfast = 518
-Dublin to Belfast = 141";
+@"111221";
 
 var smallest = "";
 
@@ -42,44 +13,22 @@ var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
-var distances = new Dictionary<(string, string), int>();
-
-foreach (var line in input.Split(Environment.NewLine))
+for (int j = 0; j < 40; j++)
 {
-    var split = line.Split(' ');
-    var key = (split[0], split[2]);
-    var value = int.Parse(split[4]);
-    distances.Add(key, value);
-}
-var cities = distances.Keys.SelectMany(key => new[] { key.Item1, key.Item2 }).Distinct().ToList();
-
-var permutations = GetPermutations(cities, cities.Count).Select(x => x.ToArray()).ToList();
-IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-{
-    if (length == 1) return list.Select(t => new T[] { t });
-
-    return GetPermutations(list, length - 1)
-        .SelectMany(t => list.Where(e => !t.Contains(e)),
-            (t1, t2) => t1.Concat(new T[] { t2 }));
-}
-
-result = permutations.Select(GetDistance).Max();
-int GetDistance(string[] cities)
-{
-    var count = 0;
-    for (var i = 0; i < cities.Length - 1; i++)
+    var targetString = "";
+    for (int i = 0; i < input.Length; i++)
     {
-        if (distances.ContainsKey((cities[i], cities[i + 1])))
-        {
-            count += distances[(cities[i], cities[i + 1])];
-        }
-        else
-        {
-            count += distances[(cities[i + 1], cities[i])];
-        }
+        var character = input[i];
+        var chunk = input.Skip(i).TakeWhile(x => x == input[i]);
+        var count = chunk.Count();
+        i += count - 1;
+        targetString += count.ToString();
+        targetString += character.ToString();
     }
-    return count;
+    input = targetString;
 }
+
+result = input.Length;
 timer.Stop();
 Console.WriteLine(result);
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
