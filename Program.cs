3894538580d1ea01c7086411e771ed181f11ae36
Jan Marques <Jan.Marques@mediahuis.be>
2024-12-10 15:29:38 +0100

    progress

--- Program.cs
+++ Program.cs
@@ -1,443 +1,331 @@
 ﻿var fullInput =
-@"bn RSHIFT 2 -> bo
-lf RSHIFT 1 -> ly
-fo RSHIFT 3 -> fq
-cj OR cp -> cq
-fo OR fz -> ga
-t OR s -> u
-lx -> a
-NOT ax -> ay
-he RSHIFT 2 -> hf
-lf OR lq -> lr
-lr AND lt -> lu
-dy OR ej -> ek
-1 AND cx -> cy
-hb LSHIFT 1 -> hv
-1 AND bh -> bi
-ih AND ij -> ik
-c LSHIFT 1 -> t
-ea AND eb -> ed
-km OR kn -> ko
-NOT bw -> bx
-ci OR ct -> cu
-NOT p -> q
-lw OR lv -> lx
-NOT lo -> lp
-fp OR fv -> fw
-o AND q -> r
-dh AND dj -> dk
-ap LSHIFT 1 -> bj
-bk LSHIFT 1 -> ce
-NOT ii -> ij
-gh OR gi -> gj
-kk RSHIFT 1 -> ld
-lc LSHIFT 1 -> lw
-lb OR la -> lc
-1 AND am -> an
-gn AND gp -> gq
-lf RSHIFT 3 -> lh
-e OR f -> g
-lg AND lm -> lo
-ci RSHIFT 1 -> db
-cf LSHIFT 1 -> cz
-bn RSHIFT 1 -> cg
-et AND fe -> fg
-is OR it -> iu
-kw AND ky -> kz
-ck AND cl -> cn
-bj OR bi -> bk
-gj RSHIFT 1 -> hc
-iu AND jf -> jh
-NOT bs -> bt
-kk OR kv -> kw
-ks AND ku -> kv
-hz OR ik -> il
-b RSHIFT 1 -> v
-iu RSHIFT 1 -> jn
-fo RSHIFT 5 -> fr
-be AND bg -> bh
-ga AND gc -> gd
-hf OR hl -> hm
-ld OR le -> lf
-as RSHIFT 5 -> av
-fm OR fn -> fo
-hm AND ho -> hp
-lg OR lm -> ln
-NOT kx -> ky
-kk RSHIFT 3 -> km
-ek AND em -> en
-NOT ft -> fu
-NOT jh -> ji
-jn OR jo -> jp
-gj AND gu -> gw
-d AND j -> l
-et RSHIFT 1 -> fm
-jq OR jw -> jx
-ep OR eo -> eq
-lv LSHIFT 15 -> lz
-NOT ey -> ez
-jp RSHIFT 2 -> jq
-eg AND ei -> ej
-NOT dm -> dn
-jp AND ka -> kc
-as AND bd -> bf
-fk OR fj -> fl
-dw OR dx -> dy
-lj AND ll -> lm
-ec AND ee -> ef
-fq AND fr -> ft
-NOT kp -> kq
-ki OR kj -> kk
-cz OR cy -> da
-as RSHIFT 3 -> au
-an LSHIFT 15 -> ar
-fj LSHIFT 15 -> fn
-1 AND fi -> fj
-he RSHIFT 1 -> hx
-lf RSHIFT 2 -> lg
-kf LSHIFT 15 -> kj
-dz AND ef -> eh
-ib OR ic -> id
-lf RSHIFT 5 -> li
-bp OR bq -> br
-NOT gs -> gt
-fo RSHIFT 1 -> gh
-bz AND cb -> cc
-ea OR eb -> ec
-lf AND lq -> ls
-NOT l -> m
-hz RSHIFT 3 -> ib
-NOT di -> dj
-NOT lk -> ll
-jp RSHIFT 3 -> jr
-jp RSHIFT 5 -> js
-NOT bf -> bg
-s LSHIFT 15 -> w
-eq LSHIFT 1 -> fk
-jl OR jk -> jm
-hz AND ik -> im
-dz OR ef -> eg
-1 AND gy -> gz
-la LSHIFT 15 -> le
-br AND bt -> bu
-NOT cn -> co
-v OR w -> x
-d OR j -> k
-1 AND gd -> ge
-ia OR ig -> ih
-NOT go -> gp
-NOT ed -> ee
-jq AND jw -> jy
-et OR fe -> ff
-aw AND ay -> az
-ff AND fh -> fi
-ir LSHIFT 1 -> jl
-gg LSHIFT 1 -> ha
-x RSHIFT 2 -> y
-db OR dc -> dd
-bl OR bm -> bn
-ib AND ic -> ie
-x RSHIFT 3 -> z
-lh AND li -> lk
-ce OR cd -> cf
-NOT bb -> bc
-hi AND hk -> hl
-NOT gb -> gc
-1 AND r -> s
-fw AND fy -> fz
-fb AND fd -> fe
-1 AND en -> eo
-z OR aa -> ab
-bi LSHIFT 15 -> bm
-hg OR hh -> hi
-kh LSHIFT 1 -> lb
-cg OR ch -> ci
-1 AND kz -> la
-gf OR ge -> gg
-gj RSHIFT 2 -> gk
-dd RSHIFT 2 -> de
-NOT ls -> lt
-lh OR li -> lj
-jr OR js -> jt
-au AND av -> ax
-0 -> c
-he AND hp -> hr
-id AND if -> ig
-et RSHIFT 5 -> ew
-bp AND bq -> bs
-e AND f -> h
-ly OR lz -> ma
-1 AND lu -> lv
-NOT jd -> je
-ha OR gz -> hb
-dy RSHIFT 1 -> er
-iu RSHIFT 2 -> iv
-NOT hr -> hs
-as RSHIFT 1 -> bl
-kk RSHIFT 2 -> kl
-b AND n -> p
-ln AND lp -> lq
-cj AND cp -> cr
-dl AND dn -> do
-ci RSHIFT 2 -> cj
-as OR bd -> be
-ge LSHIFT 15 -> gi
-hz RSHIFT 5 -> ic
-dv LSHIFT 1 -> ep
-kl OR kr -> ks
-gj OR gu -> gv
-he RSHIFT 5 -> hh
-NOT fg -> fh
-hg AND hh -> hj
-b OR n -> o
-jk LSHIFT 15 -> jo
-gz LSHIFT 15 -> hd
-cy LSHIFT 15 -> dc
-kk RSHIFT 5 -> kn
-ci RSHIFT 3 -> ck
-at OR az -> ba
-iu RSHIFT 3 -> iw
-ko AND kq -> kr
-NOT eh -> ei
-aq OR ar -> as
-iy AND ja -> jb
-dd RSHIFT 3 -> df
-bn RSHIFT 3 -> bp
-1 AND cc -> cd
-at AND az -> bb
-x OR ai -> aj
-kk AND kv -> kx
-ao OR an -> ap
-dy RSHIFT 3 -> ea
-x RSHIFT 1 -> aq
-eu AND fa -> fc
-kl AND kr -> kt
-ia AND ig -> ii
-df AND dg -> di
-NOT fx -> fy
-k AND m -> n
-bn RSHIFT 5 -> bq
-km AND kn -> kp
-dt LSHIFT 15 -> dx
-hz RSHIFT 2 -> ia
-aj AND al -> am
-cd LSHIFT 15 -> ch
-hc OR hd -> he
-he RSHIFT 3 -> hg
-bn OR by -> bz
-NOT kt -> ku
-z AND aa -> ac
-NOT ak -> al
-cu AND cw -> cx
-NOT ie -> if
-dy RSHIFT 2 -> dz
-ip LSHIFT 15 -> it
-de OR dk -> dl
-au OR av -> aw
-jg AND ji -> jj
-ci AND ct -> cv
-dy RSHIFT 5 -> eb
-hx OR hy -> hz
-eu OR fa -> fb
-gj RSHIFT 3 -> gl
-fo AND fz -> gb
-1 AND jj -> jk
-jp OR ka -> kb
-de AND dk -> dm
-ex AND ez -> fa
-df OR dg -> dh
-iv OR jb -> jc
-x RSHIFT 5 -> aa
-NOT hj -> hk
-NOT im -> in
-fl LSHIFT 1 -> gf
-hu LSHIFT 15 -> hy
-iq OR ip -> ir
-iu RSHIFT 5 -> ix
-NOT fc -> fd
-NOT el -> em
-ck OR cl -> cm
-et RSHIFT 3 -> ev
-hw LSHIFT 1 -> iq
-ci RSHIFT 5 -> cl
-iv AND jb -> jd
-dd RSHIFT 5 -> dg
-as RSHIFT 2 -> at
-NOT jy -> jz
-af AND ah -> ai
-1 AND ds -> dt
-jx AND jz -> ka
-da LSHIFT 1 -> du
-fs AND fu -> fv
-jp RSHIFT 1 -> ki
-iw AND ix -> iz
-iw OR ix -> iy
-eo LSHIFT 15 -> es
-ev AND ew -> ey
-ba AND bc -> bd
-fp AND fv -> fx
-jc AND je -> jf
-et RSHIFT 2 -> eu
-kg OR kf -> kh
-iu OR jf -> jg
-er OR es -> et
-fo RSHIFT 2 -> fp
-NOT ca -> cb
-bv AND bx -> by
-u LSHIFT 1 -> ao
-cm AND co -> cp
-y OR ae -> af
-bn AND by -> ca
-1 AND ke -> kf
-jt AND jv -> jw
-fq OR fr -> fs
-dy AND ej -> el
-NOT kc -> kd
-ev OR ew -> ex
-dd OR do -> dp
-NOT cv -> cw
-gr AND gt -> gu
-dd RSHIFT 1 -> dw
-NOT gw -> gx
-NOT iz -> ja
-1 AND io -> ip
-NOT ag -> ah
-b RSHIFT 5 -> f
-NOT cr -> cs
-kb AND kd -> ke
-jr AND js -> ju
-cq AND cs -> ct
-il AND in -> io
-NOT ju -> jv
-du OR dt -> dv
-dd AND do -> dq
-b RSHIFT 2 -> d
-jm LSHIFT 1 -> kg
-NOT dq -> dr
-bo OR bu -> bv
-gk OR gq -> gr
-he OR hp -> hq
-NOT h -> i
-hf AND hl -> hn
-gv AND gx -> gy
-x AND ai -> ak
-bo AND bu -> bw
-hq AND hs -> ht
-hz RSHIFT 1 -> is
-gj RSHIFT 5 -> gm
-g AND i -> j
-gk AND gq -> gs
-dp AND dr -> ds
-b RSHIFT 3 -> e
-gl AND gm -> go
-gl OR gm -> gn
-y AND ae -> ag
-hv OR hu -> hw
-46065 -> b
-ab AND ad -> ae
-NOT ac -> ad
-1 AND ht -> hu
-NOT hn -> ho";
+""""
+"qxfcsmh"
+"ffsfyxbyuhqkpwatkjgudo"
+"byc\x9dyxuafof\\\xa6uf\\axfozomj\\olh\x6a"
+"jtqvz"
+"uzezxa\"jgbmojtwyfbfguz"
+"vqsremfk\x8fxiknektafj"
+"wzntebpxnnt\"vqndz\"i\x47vvjqo\""
+"higvez\"k\"riewqk"
+"dlkrbhbrlfrp\\damiauyucwhty"
+"d\""
+"qlz"
+"ku"
+"yy\"\"uoao\"uripabop"
+"saduyrntuswlnlkuppdro\\sicxosted"
+"tj"
+"zzphopswlwdhebwkxeurvizdv"
+"xfoheirjoakrpofles\"nfu"
+"q\xb7oh\"p\xce\"n"
+"qeendp\"ercwgywdjeylxcv"
+"dcmem"
+"\"i\x13r\"l"
+"ikso\xdcbvqnbrjduh\"uqudzki\xderwk"
+"wfdsn"
+"pwynglklryhtsqbno"
+"hcoj\x63iccz\"v\"ttr"
+"zf\x23\\hlj\\kkce\\d\\asy\"yyfestwcdxyfj"
+"xs"
+"m\"tvltapxdvtrxiy"
+"bmud"
+"k\"a"
+"b\"oas"
+"\"yexnjjupoqsxyqnquy\"uzfdvetqrc"
+"vdw\xe3olxfgujaj"
+"qomcxdnd\"\\cfoe\""
+"fpul"
+"m\"avamefphkpv"
+"vvdnb\\x\\uhnxfw\"dpubfkxfmeuhnxisd"
+"hey\\"
+"ldaeigghlfey"
+"eure\"hoy\xa5iezjp\\tm"
+"yygb\"twbj\\r\"\x10gmxuhmp\""
+"weirebp\x39mqonbtmfmd"
+"ltuz\\hs\"e"
+"ysvmpc"
+"g\x8amjtt\"megl\"omsaihifwa"
+"yimmm"
+"iiyqfalh"
+"cwknlaaf"
+"q\x37feg\xc6s\"xx"
+"uayrgeurgyp\\oi"
+"xhug\"pt\"axugllbdiggzhvy"
+"kdaarqmsjfx\xc3d"
+"\"vkwla"
+"d\""
+"tmroz\"bvfinxoe\\mum\"wmm"
+"\"n\"bbswxne\\p\\yr\"qhwpdd"
+"skzlkietklkqovjhvj\xfe"
+"pbg\\pab\"bubqaf\"obzcwxwywbs\\dhtq"
+"xxjidvqh\"lx\\wu\"ij"
+"daef\x5fe\x5b\\kbeeb\x13qnydtboof"
+"ogvazaqy\"j\x73"
+"y"
+"n\"tibetedldy\\gsamm\"nwu"
+"wldkvgdtqulwkad"
+"dpmxnj"
+"twybw\"cdvf\"mjdajurokbce"
+"ru\"\\lasij\"i"
+"roc\\vra\\lhrm"
+"pbkt\x60booz\"fjlkc"
+"j\x4dytvjwrzt"
+"\\uiwjkniumxcs"
+"cbhm\"nexccior\"v\"j\"nazxilmfp\x47"
+"qdxngevzrlgoq"
+"\"lrzxftytpobsdfyrtdqpjbpuwmm\x9e"
+"mdag\x0asnck\xc2ggj\"slb\"fjy"
+"wyqkhjuazdtcgkcxvjkpnjdae"
+"aixfk\xc0iom\x21vueob"
+"dkiiakyjpkffqlluhaetires"
+"ysspv\"lysgkvnmwbbsy"
+"gy\"ryexcjjxdm\"xswssgtr"
+"s"
+"ddxv"
+"qwt\"\x27puilb\"pslmbrsxhrz"
+"qdg\xc9e\\qwtknlvkol\x54oqvmchn\\"
+"lvo"
+"b"
+"fk\"aa\"\"yenwch\\\\on"
+"srig\x63hpwaavs\\\x80qzk\"xa\"\xe6u\\wr"
+"yxjxuj\"ghyhhxfj\"\xa6qvatre"
+"yoktqxjxkzrklkoeroil"
+"\"jfmik\""
+"smgseztzdwldikbqrh\""
+"jftahgctf\"hoqy"
+"tcnhicr\"znpgckt\"ble"
+"vqktnkodh\"lo\"a\\bkmdjqqnsqr"
+"ztnirfzqq"
+"s"
+"xx"
+"iqj\"y\\hqgzflwrdsusasekyrxbp\\ad"
+"\\xzjhlaiynkioz\"\"bxepzimvgwt"
+"s\x36rbw"
+"mniieztwrisvdx"
+"atyfxioy\x2b\\"
+"irde\x85\x5cvbah\\jekw\"ia"
+"bdmftlhkwrprmpat\"prfaocvp"
+"w\\k"
+"umbpausy"
+"zfauhpsangy"
+"p\"zqyw"
+"wtztypyqvnnxzvlvipnq\"zu"
+"deicgwq\\oqvajpbov\\or\"kgplwu"
+"mbzlfgpi\\\\zqcidjpzqdzxityxa"
+"lfkxvhma"
+"\xf2yduqzqr\"\\fak\"p\"n"
+"mpajacfuxotonpadvng"
+"anb\\telzvcdu\\a\xf2flfq"
+"lrs\"ebethwpmuuc\"\x86ygr"
+"qmvdbhtumzc\"ci"
+"meet"
+"yopg\x0fdxdq\"h\\ugsu\xffmolxjv"
+"uhy"
+"fzgidrtzycsireghazscvmwcfmw\\t"
+"cqohkhpgvpru"
+"bihyigtnvmevx\"xx"
+"xz"
+"zofomwotzuxsjk\"q\"mc\"js\"dnmalhxd"
+"\\ktnddux\\fqvt\"ibnjntjcbn"
+"ia"
+"htjadnefwetyp\xd5kbrwfycbyy"
+"\"\\hkuxqddnao"
+"meqqsz\x83luecpgaem"
+"cvks\x87frvxo\"svqivqsdpgwhukmju"
+"sgmxiai\\o\"riufxwjfigr\xdf"
+"fgywdfecqufccpcdn"
+"faghjoq\x28abxnpxj"
+"zuppgzcfb\"dctvp\"elup\"zxkopx"
+"xqs\x45xxdqcihbwghmzoa"
+"anbnlp\\cgcvm\"hc"
+"xf\"fgrngwzys"
+"nrxsjduedcy\x24"
+"\x71sxl\"gj\"sds\"ulcruguz\\t\\ssvjcwhi"
+"jhj\"msch"
+"qpovolktfwyiuyicbfeeju\x01"
+"nkyxmb\"qyqultgt\"nmvzvvnxnb"
+"ycsrkbstgzqb\"uv\\cisn"
+"s"
+"ueptjnn\"\"sh"
+"lp\"z\"d\"mxtxiy"
+"yzjtvockdnvbubqabjourf\"k\"uoxwle"
+"\x82\"wqm\""
+"\xb5cwtuks\x5fpgh"
+"wd"
+"tbvf"
+"ttbmzdgn"
+"vfpiyfdejyrlbgcdtwzbnm"
+"uc"
+"otdcmhpjagqix"
+"\\\xb1qso\"s"
+"scowax"
+"behpstjdh\xccqlgnqjyz\"eesn"
+"r\xe1cbnjwzveoomkzlo\\kxlfouhm"
+"jgrl"
+"kzqs\\r"
+"ctscb\x7fthwkdyko\"\x62pkf\"d\xe6knmhurg"
+"tc\"kw\x3ftt"
+"bxb\x5ccl"
+"jyrmfbphsldwpq"
+"jylpvysl\"\"juducjg"
+"en\\m\"kxpq\"wpb\\\""
+"madouht\"bmdwvnyqvpnawiphgac\""
+"vuxpk\"ltucrw"
+"aae\x60arr"
+"ttitnne\"kilkrgssnr\xfdurzh"
+"oalw"
+"pc\"\"gktkdykzbdpkwigucqni\"nxiqx"
+"dbrsaj"
+"bgzsowyxcbrvhtvekhsh\"qgd"
+"kudfemvk\"\"\"hkbrbil\"chkqoa"
+"zjzgj\\ekbhyfzufy"
+"\\acos\"fqekuxqzxbmkbnn\x1ejzwrm"
+"elxahvudn\"txtmomotgw"
+"\x2eoxmwdhelpr\"cgi\xf7pzvb"
+"eapheklx"
+"hfvma\"mietvc\"tszbbm\"czex"
+"h\"iiockj\\\xc1et"
+"d\"rmjjftm"
+"qlvhdcbqtyrhlc\\"
+"yy\"rsucjtulm\"coryri\"eqjlbmk"
+"tv"
+"r\"bfuht\\jjgujp\""
+"kukxvuauamtdosngdjlkauylttaokaj"
+"srgost\"\"rbkcqtlccu\x65ohjptstrjkzy"
+"yxwxl\\yjilwwxffrjjuazmzjs"
+"dxlw\\fkstu\"hjrtiafhyuoh\"sewabne"
+"\x88sj\"v"
+"rfzprz\xec\"oxqclu\"krzefp\\q"
+"cfmhdbjuhrcymgxpylllyvpni"
+"ucrmjvmimmcq\x88\xd9\"lz"
+"lujtt\""
+"gvbqoixn\"pmledpjmo\"flydnwkfxllf"
+"dvxqlbshhmelsk\x8big\"l"
+"mx\x54lma\x8bbguxejg"
+"\x66jdati\xeceieo"
+"\"iyyupixei\x54ff"
+"xohzf\"rbxsoksxamiu"
+"vlhthspeshzbppa\x4drhqnohjop\"\"mfjd"
+"f\"tvxxla\"vurian\"\"idjq\x3aptm\xc3olep"
+"gzqz"
+"kbq\\wogye\\altvi\\hbvmodny"
+"j\xd8"
+"ofjozdhkblvndl"
+"hbitoupimbawimxlxqze"
+"ypeleimnme"
+"xfwdrzsc\\oxqamawyizvi\\y"
+"enoikppx\xa1ixe\"yo\"gumye"
+"fb"
+"vzf"
+"zxidr"
+"cu\x31beirsywtskq"
+"lxpjbvqzztafwezd"
+"\\jyxeuo\x18bv"
+"b\"vawc\"p\\\\giern\"b"
+"odizunx\"\"t\\yicdn\"x\"sdiz"
+"\"\"tebrtsi"
+"ctyzsxv\xa6pegfkwsi\"tgyltaakytccb"
+"htxwbofchvmzbppycccliyik\xe5a"
+"ggsslefamsklezqkrd"
+"rcep\"fnimwvvdx\"l"
+"zyrzlqmd\x12egvqs\\llqyie"
+"\x07gsqyrr\\rcyhyspsvn"
+"butg\""
+"gb"
+"gywkoxf\"jsg\\wtopxvumirqxlwz"
+"rj\"ir\"wldwveair\x2es\"dhjrdehbqnzl"
+"ru\"elktnsbxufk\\ejufjfjlevt\\lrzd"
+"\"widsvok"
+"oy\"\x81nuesvw"
+"ay"
+"syticfac\x1cfjsivwlmy\"pumsqlqqzx"
+"m"
+"rjjkfh\x78cf\x2brgceg\"jmdyas\"\\xlv\xb6p"
+"tmuvo\"\x3ffdqdovjmdmkgpstotojkv\"as"
+"jd\\ojvynhxllfzzxvbn\"wrpphcvx"
+"pz"
+"\"twr"
+"n\\hdzmxe\"mzjjeadlz"
+"fb\"rprxuagvahjnri"
+"rfmexmjjgh\\xrnmyvnatrvfruflaqjnd"
+"obbbde\"co\"qr\"qpiwjgqahqm\\jjp\""
+"vpbq\"\"y\"czk\\b\x52ed\"lnzepobp"
+"syzeajzfarplydipny\"y\"\xe8ad"
+"mpyodwb"
+"\x47rakphlqqptd"
+"wa\"oj\"aiy"
+"a"
+"ropozx"
+"q\x51nbtlwa"
+"etukvgx\\jqxlkq"
+"\"tp\"rah\"pg\"s\"bpdtes\\tkasdhqd"
+"dn\"qqpkikadowssb\xcah\"dzpsf\\ect\"jdh"
+"pxunovbbrrn\\vullyn\"bno\"\"\"myfxlp\""
+"qaixyazuryvkmoulhcqaotegfj\\mpzm"
+"bvfrbicutzbjwn\\oml\"cf\"d\"ezcpv\"j"
+"rmbrdtneudemigdhelmb"
+"aq\\aurmbhy"
+"wujqvzw"
+"gf\"tssmvm\"gm\"hu\x9a\xb7yjawsa"
+"hrhqqxow\xe2gsydtdspcfqy\"zw\\ou"
+"ianwwf\\yko\\tdujhhqdi"
+"xylz\"zpvpab"
+"lwuopbeeegp"
+"aoop\x49jhhcexdmdtun"
+"\\\\mouqqcsgmz"
+"tltuvwhveau\x43b\"ymxjlcgiymcynwt"
+"gsugerumpyuhtjljbhrdyoj"
+"lnjm\xb8wg\"ajh"
+"zmspue\"nfttdon\\b\"eww"
+"\"w\x67jwaq\x7ernmyvs\\rmdsuwydsd\"th"
+"ogtgvtlmcvgllyv"
+"z\"fqi\"rvddoehrciyl"
+"yustxxtot\"muec\"xvfdbzunzvveq"
+"mqslw"
+"txqnyvzmibqgjs\xb6xy\x86nfalfyx"
+"kzhehlmkholov"
+"plpmywcnirrjutjguosh\\"
+"pydbnqofv\"dn\\m"
+"aegqof"
+"eambmxt\\dxagoogl\\zapfwwlmk"
+"afbmqitxxqhddlozuxcpjxgh"
+"vgts"
+"bfdpqtoxzzhmzcilehnflna"
+"s\"idpz"
+"\xcfhgly\"nlmztwybx\"ecezmsxaqw"
+"aackfgndqcqiy"
+"\x22unqdlsrvgzfaohoffgxzfpir\"s"
+"abh\"ydv\"kbpdhrerl"
+"bdzpg"
+"ekwgkywtmzp"
+"wtoodejqmrrgslhvnk\"pi\"ldnogpth"
+"njro\x68qgbx\xe4af\"\\suan"
+"""";
 
 var smallInput =
-@"123 -> x
-456 -> y
-x AND y -> d
-x OR y -> e
-x LSHIFT 2 -> f
-y RSHIFT 2 -> g
-NOT x -> h
-NOT y -> i";
+""""
+""
+"abc"
+"aaa\"aaa"
+"\x27"
+"""";
 
 var smallest = "";
 
 var input = smallInput;
-input = fullInput;
+//input = fullInput;
 //input = smallest;
-
 var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
-var multiOperations = new Dictionary<string, Func<ushort, ushort, ushort>>()
+foreach (var line in input.Split(Environment.NewLine))
 {
-    { "OR", Or },
-    { "AND", And },
-    { "RSHIFT", Rshift},
-    { "LSHIFT", Lshift }
-};
-
-var memory = new Dictionary<string, ushort>();
-
-ushort And(ushort arg1, ushort arg2) => (ushort)(arg1 & arg2);
-ushort Rshift(ushort arg1, ushort arg2) => (ushort)(arg1 >> arg2);
-ushort Lshift(ushort arg1, ushort arg2) => (ushort)(arg1 << arg2);
-ushort Or(ushort arg1, ushort arg2) => (ushort)(arg1 | arg2);
-ushort Not(ushort a) => (ushort)~a;
-
-{
-
+    result += line.Length;
+    var tmp = line.Replace("\\\"", "\\x00").Replace("\\\\", "aa");
+    result -= line.Length - 2 - line.Replace("\\\"", "\\x00").Replace("\\\\", "aa").Count(x => x == '\\') * 3;
 }
 
-memory["b"] = 46065;
-for (int i = 0; i < 10000; i++)
-//while (true)
-{
-    foreach (var line in input.Split(Environment.NewLine))
-    {
-        var arrow = line.Split(" -> ");
-        var target = arrow[1];
-        var split = arrow[0].Split(' ');
-        var multiOp = multiOperations.SingleOrDefault(x => arrow[0].Contains(x.Key));
-
-        if (multiOp.Value != default)
-        {
-            var a = GetValue(split[0]);
-            if (!a.HasValue) { continue; }
-            var b = GetValue(split[2]);
-            if (!b.HasValue) { continue; }
-
-            var opResult = multiOp.Value(a.Value, b.Value);
-            SetValue(target, opResult);
-        }
-        else if (arrow[0].Contains("NOT"))
-        {
-            var a = GetValue(split[1]);
-            if (!a.HasValue) { continue; }
-
-            var opResult = Not(a.Value);
-            SetValue(target, opResult);
-        }
-        else
-        {
-            var a = GetValue(split[0]);
-            if (!a.HasValue) { continue; }
-
-            SetValue(target, a.Value);
-        }
-    }
-}
-
-
-void SetValue(string target, ushort opResult)
-{
-    memory[target] = opResult;
-}
-
-ushort? GetValue(string v)
-{
-    if (ushort.TryParse(v, out var value))
-    {
-        return value;
-    }
-    if (memory.ContainsKey(v))
-    {
-        return memory[v];
-    }
-    return null;
-}
-
-result = memory["a"];
 timer.Stop();
 Console.WriteLine(result);
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
