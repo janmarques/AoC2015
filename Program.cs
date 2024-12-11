618373c318ac3467a3d410c5ac0d619d7aa85e78
Jan Marques <jan@janmarques.net>
2024-12-11 10:11:52 +0100

    ss

--- Program.cs
+++ Program.cs
@@ -30,54 +30,76 @@ var smallInput =
 var smallest = "";
 
 var input = smallInput;
-input = fullInput;
+//input = fullInput;
 //input = smallest;
 var timer = System.Diagnostics.Stopwatch.StartNew();
 
 var result = 0;
 
+var xxxx = GetCombinations(new[] { new Node { Value = 1 }, new Node { Value = 2 }, new Node { Value = 2 }, new Node { Value = 3 }}).Select(x => x.ToList()).ToList();
+
 var containers = input.Split(Environment.NewLine).Select(int.Parse).Select(x => new Node { Value = x }).ToList();
 var volume = containers.Count == 5 ? 25 : 150;
 
-
-
-
-var combinations = new HashSet<string>();
-int i = 0;
-Fill(containers.ToList(), new List<Node>(), 0);
-
-void Fill(List<Node> nodesLeft, List<Node> nodesUsed, int filled, int depth = 0)
+var cache = Enumerable.Range(0, containers.Sum(x => x.Value) + 1).ToDictionary(x => x, x => new List<List<Node>>());
+//var cacheKeyed = Enumerable.Range(0, volume).ToDictionary(x => x, x => new List<string>());
+foreach (var container in containers)
 {
-    i++;
-    //if (relevants.Sum(x => x.Value) + filled < volume) { return; }
-    foreach (var item in nodesLeft)
+    cache[container.Value].Add(new List<Node> { container });
+}
+
+for (int i = 1; i <= volume; i++)
+{
+    var relevant = cache.Where(x => x.Key < i && x.Value.Any()).SelectMany(x => x.Value.Select(y => (x.Key, y))).ToList();
+    //var permutations2 = GetAllPermutations(relevant).ToList();
+    var permutations = GetAllPermutations(relevant).Select(x => x.ToList()).Select(x => (value: x, hash: Hash(x.SelectMany(y => y.y)))).DistinctBy(x => x.hash).ToList();
+    foreach (var permutation in permutations)
     {
-        if (depth < 4)
-        {
-            Console.WriteLine($"{item.Value} d {depth} {i}");
-        }
-        var newFilled = filled + item.Value;
-        if (newFilled > volume)
-        {
-            continue;
-        }
+        var sum = permutation.value.Sum(x => x.Key);
+        cache[sum].Add(permutation.value.SelectMany(x => x.y).ToList());
+    }
 
-        var cloneUsed = nodesUsed.ToList();
-        cloneUsed.Add(item);
-        if (newFilled == volume)
-        {
-            combinations.Add(string.Join("|", cloneUsed.Select(x => x.GetHashCode()).OrderBy(x => x)));
-            continue;
-        }
+    if (i == 11)
+    {
 
-        var cloneLeft = nodesLeft.Where(x => x != item).ToList();
-
-        Fill(cloneUsed, cloneLeft, newFilled, depth + 1);
     }
 }
 
-result = combinations.Count;
+string Hash(IEnumerable<Node> nodes) => string.Join("|", nodes.Select(x => x.GetHashCode()).OrderBy(x => x));
 
+IEnumerable<IEnumerable<T>> GetAllPermutations<T>(IEnumerable<T> list)
+{
+    var count = list.Count();
+    //if(count == 0) { yield break;}
+    yield return list;
+    for (int i = 2; i <= count; i++)
+    {
+        foreach (var item in GetPermutations(list, i).ToList())
+        
+        {
+            yield return item;
+        }
+    }
+}
+
+IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+{
+    if (length == 1) return list.Select(t => new T[] { t });
+
+    return GetPermutations(list, length - 1)
+        .SelectMany(t => list.Where(e => !t.Contains(e)),
+            (t1, t2) => t1.Concat(new T[] { t2 }));
+}
+
+// https://stackoverflow.com/questions/64998630/get-all-combinations-of-liststring-where-order-doesnt-matter-and-minimum-of-2
+IEnumerable<T[]> GetCombinations<T>(T[] source)
+{
+    for (var i = 0; i < (1 << source.Count()); i++)
+        yield return source
+           .Where((t, j) => (i & (1 << j)) != 0)
+           .ToArray();
+
+}
 timer.Stop();
 Console.WriteLine(result);
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
@@ -86,4 +108,8 @@ Console.ReadLine();
 class Node
 {
     public int Value { get; set; }
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
 }
