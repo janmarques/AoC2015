3ee7bf9f002bbca1d62af89a0cbfd4bbf2a9d929
Jan Marques <jan@janmarques.net>
2024-12-11 15:46:18 +0100

    d21p1

--- Day (20).cs
+++ Day (20).cs
@@ -1,4 +1,6 @@
-﻿//var fullInput =
+﻿//using System.Runtime.ExceptionServices;
+
+//var fullInput =
 //@"";
 
 //var smallInput =
@@ -7,15 +9,64 @@
 //var smallest = "";
 
 //var input = smallInput;
-////var input = fullInput;
-////var input = smallest;
+////input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
 
-//var result = "";
+//var result = 0;
 
 //foreach (var line in input.Split(Environment.NewLine))
 //{
 
 //}
 
+//int i = 665280;
+//while (true)
+//{
+//    var xx = PresentsForHouse(i);
+//    if (xx > 29000000)
+//    {
+//        result = i;
+//        break;
+//    }
+//    i++;
+//}
+//// 705600?
+//int PresentsForHouse(int number)
+//{
+//    var sum = 0;
+//    foreach (var item in GetFactors(number))
+//    {
+//        if (item * 50 >= number)
+//        {
+//            sum += item * 11;
+//        }
+//    }
+//    return sum;
+//}
+
+//IEnumerable<int> GetFactors(int number)
+//{
+//    for (int i = 1; i <= number / 2; i++)
+//    {
+//        if (number % i == 0) { yield return i; }
+//    }
+//    yield return number;
+//}
+
+//timer.Stop();
 //Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
 //Console.ReadLine();
+
+//void PrintGrid<T>(T[][] grid)
+//{
+//    for (int i = 0; i < grid.Length; i++)
+//    {
+//        for (int j = 0; j < grid[i].Length; j++)
+//        {
+//            Console.Write(grid[i][j]);
+//        }
+//        Console.WriteLine();
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿using System.Runtime.ExceptionServices;
-
-var fullInput =
+﻿var fullInput =
 @"";
 
 var smallInput =
-@"";
+@"Weapons:    Cost  Damage  Armor
+Dagger        8     4       0
+Shortsword   10     5       0
+Warhammer    25     6       0
+Longsword    40     7       0
+Greataxe     74     8       0
+
+Armor:      Cost  Damage  Armor
+Leather      13     0       1
+Chainmail    31     0       2
+Splintmail   53     0       3
+Bandedmail   75     0       4
+Platemail   102     0       5
+
+Rings:      Cost  Damage  Armor
+Damage +1    25     1       0
+Damage +2    50     2       0
+Damage +3   100     3       0
+Defense +1   20     0       1
+Defense +2   40     0       2
+Defense +3   80     0       3";
 
 var smallest = "";
 
@@ -13,46 +31,72 @@ var input = smallInput;
 //input = smallest;
 var timer = System.Diagnostics.Stopwatch.StartNew();
 
-var result = 0;
+var result = int.MaxValue;
 
-foreach (var line in input.Split(Environment.NewLine))
+var lines = input.Split(Environment.NewLine);
+var weapons = lines[1..6].Select(ToItem).ToList();
+var armors = lines[8..13].Select(ToItem).ToList();
+armors.Add(new Item(0, 0, 0));
+var rings = lines[15..].Select(ToItem).ToList();
+rings.Add(new Item(0, 0, 0));
+rings.Add(new Item(0, 0, 0));
+
+Item ToItem(string arg)
 {
-
+    var split = arg.Split("  ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
+    return new Item(split.ElementAt(1), split.ElementAt(2), split.ElementAt(3));
 }
 
-int i = 665280;
-while (true)
+//var me = new Player { Name = "me", Hp = 8, Damage = 5, Armor = 5 };
+//var boss = new Player { Name = "boss", Hp = 12, Damage = 7, Armor = 2 };
+
+
+Player CreateBoss() => new Player { Name = "boss", Hp = 103, Damage = 9, Armor = 2 };
+Player CreateMe() => new Player { Name = "me", Hp = 100, Damage = 0, Armor = 0 };
+
+foreach (var weapon in weapons)
+    foreach (var armor in armors)
+        foreach (var ring1 in rings)
+            foreach (var ring2 in rings)
+            {
+                if (ring1 == ring2) { continue; }
+                var items = new[] { weapon, armor, ring1, ring2 };
+                var boss = CreateBoss();
+                var me = CreateMe();
+                me.Armor += items.Sum(x => x.armor);
+                me.Damage += items.Sum(x => x.damage);
+                var cost = items.Sum(x => x.cost);
+                var win = SimulateFight(me, boss);
+                if (win)
+                {
+                    result = Math.Min(cost, result);
+                }
+            }
+
+
+bool SimulateFight(Player me, Player boss)
 {
-    var xx = PresentsForHouse(i);
-    if (xx > 29000000)
+    while (true)
     {
-        result = i;
-        break;
-    }
-    i++;
-}
-// 705600?
-int PresentsForHouse(int number)
-{
-    var sum = 0;
-    foreach (var item in GetFactors(number))
-    {
-        if (item * 50 >= number)
+        Attack(me, boss);
+        if (boss.Hp <= 0)
         {
-            sum += item * 11;
+            return true;
+        }
+        Attack(boss, me);
+        if (me.Hp <= 0)
+        {
+            return false;
         }
     }
-    return sum;
 }
 
-IEnumerable<int> GetFactors(int number)
+void Attack(Player attacker, Player victim)
 {
-    for (int i = 1; i <= number / 2; i++)
-    {
-        if (number % i == 0) { yield return i; }
-    }
-    yield return number;
+    victim.Hp -= Math.Max(1, attacker.Damage - victim.Armor);
 }
+
+
 
 timer.Stop();
 Console.WriteLine(result);
@@ -69,4 +113,14 @@ void PrintGrid<T>(T[][] grid)
         }
         Console.WriteLine();
     }
+}
+
+record Item(int cost, int damage, int armor);
+
+class Player
+{
+    public string Name { get; set; }
+    public int Hp { get; set; }
+    public int Armor { get; set; }
+    public int Damage { get; set; }
 }
