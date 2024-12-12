d6956a5efc74853a3b9d869d89ad28cbaeb8f6fa
Jan Marques <jan@janmarques.net>
2024-12-12 06:55:48 +0100

    d22p1

--- Program.cs
+++ Program.cs
@@ -1,17 +1,9 @@
-﻿var timer = System.Diagnostics.Stopwatch.StartNew();
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
 
-var result = 0;
-var i = 0;
+var timer = System.Diagnostics.Stopwatch.StartNew();
 
-Item ToItem(string arg)
-{
-    var split = arg.Split("  ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse);
-    return new Item(split.ElementAt(1), split.ElementAt(2), split.ElementAt(3));
-}
-
-var staticSpells = new[] { "missile", "drain" };
-var effects = new[] { ("shield", 6), ("poison", 6), ("recharge", 5) };
-var spells = staticSpells.Concat(effects.Select(x => x.Item1));
+var result = int.MaxValue;
 
 var costs = new Dictionary<string, int>()
 {
@@ -22,184 +14,147 @@ var costs = new Dictionary<string, int>()
     { "recharge", 229},
 };
 
+var pqSet = new HashSet<(string spell, int myHp, int myMana, int bossHp, int shieldTurns, int poisonTurns, int rechargeTurns, int manaSpent)>();
 
-
-
-Player CreateMe() => new Player { Name = "me", Hp = 50, Mana = 500 };
-Player CreateBoss() => new Player { Name = "boss", Hp = 103, Damage = 9 };
-
-while (true)
+var pq = new PriorityQueue<(string spell, int myHp, int myMana, int bossHp, int shieldTurns, int poisonTurns, int rechargeTurns, int manaSpent), int>();
+int BOSS_DAMAGE = 8;
+foreach (var item in costs.Keys)
 {
-    var working = new List<string>()
+    var startState = (item, 50, 500, 55, 0, 0, 0, 0);
+    //var startState = (item, 10, 250, 14, 0, 0, 0, 0);
+    pq.Enqueue(startState, 0);
+}
+
+
+int i = 0;
+while (pq.Count > 0)
+{
+    var (spell, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent) = pq.Dequeue();
+    i++;
+    if (i % 100000 == 0)
     {
-"recharge",
-"poison",
-"shield",
-"recharge",
-"poison",
-"shield",
-"recharge",
-"poison",
-"shield",
-"recharge",
-"poison",
-"shield",
-"recharge",
-"poison",
-"shield",
-"missile",
-"poison",
-"missile",
-    };
-
-    working.Reverse();
-    var someSpells = new Stack<string>(working);
-    //for (int i = 0; i < 100; i++)
-    //{
-    //    someSpells.Push(spells.OrderBy(x => Guid.NewGuid()).First());
-    //}
-
-
-    var copy = someSpells.ToList();
-    var boss = CreateBoss();
-    var me = CreateMe();
-    var xxx = Simulate(someSpells, me, boss);
-    if (boss.Hp <= 0)
-    {
-
-        Console.WriteLine(me);
-        Console.WriteLine(boss);
-        Console.WriteLine($"{result} {i} {string.Join("|", copy.Take(result))}");
-        Console.WriteLine();
+        Console.WriteLine($"pq {pq.Count} state {manaSpent}");
     }
-    if (xxx)
+    var (simResult, myHpResult, myManaResult, bossHpResult, shieldTurnsResult, poisonTurnsResult, rechargeTurnsResult, manaSpentResult) = Simulate(spell, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+    if (!simResult.HasValue)
     {
-        break;
+        if (manaSpentResult > result) { continue; }
+        foreach (var item in costs.Keys)
+        {
+            switch (item)
+            {
+                case "shield":
+                    if (shieldTurnsResult > 0) { continue; }
+                    break;
+                case "poison":
+                    if (poisonTurnsResult > 0) { continue; }
+                    break;
+                case "recharge":
+                    if (rechargeTurnsResult > 0) { continue; }
+                    break;
+            }
+            if (!pqSet.Contains((item, myHpResult, myManaResult, bossHpResult, shieldTurnsResult, poisonTurnsResult, rechargeTurnsResult, manaSpentResult)))
+            {
+                pq.Enqueue((item, myHpResult, myManaResult, bossHpResult, shieldTurnsResult, poisonTurnsResult, rechargeTurnsResult, manaSpentResult), manaSpentResult);
+            }
+        }
+    }
+    else if (simResult.Value)
+    {
+        // Won
+        result = Math.Min(result, manaSpentResult);
+        //break;
+    }
+    else
+    {
+        // Lost / impossible
     }
 }
 
-// 2854 too high
-
-bool Simulate(Stack<string> chosenSpells, Player me, Player boss)
+(bool? won, int myHp, int myMana, int bossHp, int shieldTurns, int poisonTurns, int rechargeTurns, int manaSpent) Simulate(string spell, int myHp, int myMana, int bossHp, int shieldTurns, int poisonTurns, int rechargeTurns, int manaSpent)
 {
-    result = 0;
-    var activeEffects = new Dictionary<string, int>();
-    while (true)
+    var armor = 0;
+    void RunEffects()
     {
-        i++;
-        void RunEffects()
+        armor = 0;
+        if (shieldTurns > 0)
         {
-            me.Armor = 0;
-            foreach (var activeEffect in activeEffects)
-            {
-                switch (activeEffect.Key)
-                {
-                    case "shield": me.Armor = 7; break;
-                    case "poison": boss.Hp -= 3; break;
-                    case "recharge": me.Mana += 101; break;
-                    default: throw new Exception();
-                }
-                activeEffects[activeEffect.Key]--;
-            }
-            activeEffects = activeEffects.Where(x => x.Value > 0).ToDictionary();
+            armor = 7;
+            shieldTurns--;
         }
-
-        RunEffects();
-        if (boss.Hp <= 0)
+        if (poisonTurns > 0)
         {
-            return true;
+            bossHp -= 3;
+            poisonTurns--;
         }
-        if (me.Mana < costs.Min(x => x.Value))
+        if (rechargeTurns > 0)
         {
-            return false;
-        }
-        string spell;
-        do
-        {
-            spell = chosenSpells.Pop();
-        } while (costs[spell] > me.Mana || activeEffects.ContainsKey(spell));
-        Console.WriteLine($"\"{spell}\",");
-
-        me.Mana -= costs[spell];
-        result += costs[spell];
-
-        var effect = effects.SingleOrDefault(x => x.Item1 == spell);
-        if (effect != default)
-        {
-            if (activeEffects.ContainsKey(spell))
-            {
-                continue; // invalid random spell
-            }
-            else
-            {
-                activeEffects[spell] = effect.Item2;
-            }
-        }
-        else if (spell == "missile")
-        {
-            boss.Hp -= 4;
-        }
-        else if (spell == "drain")
-        {
-            boss.Hp -= 2;
-            me.Hp += 2;
-        }
-        else
-        {
-            throw new Exception();
-        }
-
-
-
-        if (boss.Hp <= 0)
-        {
-            return true;
-        }
-
-
-        RunEffects();
-        if (boss.Hp <= 0)
-        {
-            return true;
-        }
-        me.Hp -= Math.Max(1, boss.Damage - me.Armor);
-
-
-        if (me.Hp <= 0)
-        {
-            return false;
+            myMana += 101;
+            rechargeTurns--;
         }
     }
-    throw new Exception();
-}
 
+    if (costs[spell] > myMana)
+    {
+        return (false, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+    }
+
+    switch (spell)
+    {
+        case "missile":
+            bossHp -= 4;
+            break;
+        case "drain":
+            bossHp -= 2;
+            myHp += 2;
+            break;
+        case "shield":
+            if (shieldTurns > 1) { return (false, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent); }
+            shieldTurns += 6;
+            break;
+        case "poison":
+            if (poisonTurns > 1) { return (false, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent); }
+            poisonTurns += 6;
+            break;
+        case "recharge":
+            if (rechargeTurns > 1) { return (false, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent); }
+            rechargeTurns += 5;
+            break;
+        default: throw new Exception();
+    }
+
+    myMana -= costs[spell];
+    manaSpent += costs[spell];
+
+    if (bossHp <= 0)
+    {
+        return (true, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+
+    }
+
+    RunEffects();
+    if (bossHp <= 0)
+    {
+        return (true, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+    }
+    myHp -= Math.Max(1, BOSS_DAMAGE - armor);
+
+    if (myHp <= 0)
+    {
+        return (false, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+    }
+
+    RunEffects();
+    if (bossHp <= 0)
+    {
+        return (true, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+    }
+
+
+    return (null, myHp, myMana, bossHp, shieldTurns, poisonTurns, rechargeTurns, manaSpent);
+}
 
 timer.Stop();
 Console.WriteLine(result);
 Console.WriteLine(timer.ElapsedMilliseconds + "ms");
 Console.ReadLine();
-
-void PrintGrid<T>(T[][] grid)
-{
-    for (int i = 0; i < grid.Length; i++)
-    {
-        for (int j = 0; j < grid[i].Length; j++)
-        {
-            Console.Write(grid[i][j]);
-        }
-        Console.WriteLine();
-    }
-}
-
-record Item(int cost, int damage, int armor);
-
-class Player
-{
-    public string Name { get; set; }
-    public int Hp { get; set; }
-    public int Mana { get; set; }
-    public int Damage { get; set; }
-    public int Armor { get; set; }
-
-    public override string ToString() => $"{Name} {Hp}hp {Mana}m";
-}
